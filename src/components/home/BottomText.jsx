1f85072b158089e8031018ce9cace3f9b5cad3c8
Ashu Verma <ashu@Ashus-MacBook-Air.local>
2025-11-18 23:58:59 +0530

    fix bottomtext margin

--- src/components/home/BottomText.jsx
+++ src/components/home/BottomText.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const BottomText = () => {
     return (
-        <div className='text-[5.5vw] mb-5 font-[font1] flex items-center justify-center uppercase flex-row gap-2 text-center' >
+        <div className='text-[5.5vw] mb-20 lg:mb-5 font-[font1] flex items-center justify-center uppercase flex-row gap-2 text-center' >
             <div className='border-2 border-amber-50 hover:border-[#D3FD50] hover:text-[#D3FD50] rounded-full px-3 pt-2'>
                 <Link to='/projects' >Projects</Link>
             </div>
